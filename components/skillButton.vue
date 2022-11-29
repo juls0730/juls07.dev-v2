3b775e1ebad645d9d176c2ce92222b449f0a14d8
juls0730 <juliangarner240@gmail.com>
2022-11-28 22:12:48 -0600

    add skill buttons and more

---
+++ components/skillButton.vue
@@ -0,0 +1,10 @@
+<script>
+export default {
+    props: ['iconSrc', 'name']
+}
+</script>
+<template>
+    <button class="font-inter md:text-lg w-fit min-w-fit bg-zinc-800 py-1 px-2 rounded shadow flex items-center"><img
+            :src="iconSrc"
+            class="mr-2 w-5 h-5" />{{name}}</button>
+</template>
